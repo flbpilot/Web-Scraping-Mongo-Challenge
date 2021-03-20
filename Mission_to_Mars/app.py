ea5ac6db7cb7311f269ada7d1a18abefd0d3338a
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-03-19 20:57:55 -0400

    mongo error not fixed

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Create an instance of Flask
+#Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars.find_one()
+
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -25,13 +25,13 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({},scrape_mars, upsert=True)
+    mongo.db.mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/",code=302)
 
 
 if __name__ == "__main__":
